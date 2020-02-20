4a8a54ca9202f62c093fc8ad806ba96b0942853e
Matthew Denwood <md@sund.ku.dk>
2020-02-20 16:49:09 +0200

    Update runjags_version.R

--- Workshop 1 - Athens/Covariate_training/runjags_version.R
+++ Workshop 1 - Athens/Covariate_training/runjags_version.R
@@ -128,22 +128,25 @@ ones<-c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
         ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
         ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
 
-# We skip initial values here but that would be a good idea!
+# We skip other initial values here but that would be a good idea!
+inits1 = list(".RNG.name" ="base::Mersenne-Twister",
+              ".RNG.seed" = 100022)
+inits2 = list(".RNG.name" ="base::Mersenne-Twister",
+              ".RNG.seed" = 300022)
+inits3 = list(".RNG.name" ="base::Mersenne-Twister",
+              ".RNG.seed" = 500022)
 
-# run the model:
-results_without <- run.jags('model1.bug', data=list(N=N, m.short=m.short, ones=ones), monitor=c('prc','c1','c2','c3','s1','s2','s3','deviance','covs12','covs13','covs23','covc12','covc13','covc23'))
+# run the model - equivalent to the longer scripts:
+results_without <- run.jags('model1.bug', data=list(N=N, m.short=m.short, ones=ones), inits=list(inits1, inits2, inits3), monitor=c('prc','c1','c2','c3','s1','s2','s3','deviance','covs12','covs13','covs23','covc12','covc13','covc23'))
 
 results_cestode <- run.jags('model.cestode.bug', data=list(N=N, m.short=m.short, ones=ones), monitor=c('prc','c1','c2','c3','s1','s2','s3','deviance','covs12','covs13','covs23','covc12','covc13','covc23','slope','intercept'))
 
-
+# These give the main results:
 results_without
-
 results_cestode
 
 
-library('coda')
-
-# for OR and prevalence with and without covariates
+# Or we can extract e.g. slope and calculate OR and prevalence with and without covariates etc:
 slope <- combine.mcmc(results_cestode, vars='slope')
 mean(slope)
 mean(exp(as.numeric(slope))) # OR
